87e83e986d102cc828db903ae44af04b4aeb1ea4
Alejandra Vanesa Juarez D'Aquino <ale@emma.home>
2022-05-30 09:20:34 +0200

    iterate map

--- structs/main.go
+++ structs/main.go
@@ -14,15 +14,31 @@ func main() {
 		},
 	}
 	ale.setFirstName("Alejandra")
-	ale.print()
+	//ale.print()
 
 	var juan person
+	juan.setFirstName("bla")
 	//default empty strings
-	println(juan.firstName + " " + juan.lastName)
+	//println(juan.firstName + " " + juan.lastName)
 
 	colors := map[string]string{
-		"red": "#fff0000",
+		"red":   "#ff0000",
+		"blue":  "#00ff00",
+		"white": "#ffffff",
+		"green": "#0000ff",
 	}
+
+	for c, hex := range colors {
+		fmt.Printf("value c %+v hex %+v\n", c, hex)
+	}
+
+	color2 := make(map[string]string)
+
+	color2["white"] = "blabla"
+
+	delete(colors, "red")
+
 	fmt.Println(colors)
+	fmt.Println(color2)
 
 }
