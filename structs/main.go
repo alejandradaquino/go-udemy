c7040645800f12a3270a07c24e54a67555140006
Alejandra Vanesa Juarez D'Aquino <ale@emma.home>
2022-05-30 09:15:21 +0200

    map created

--- structs/main.go
+++ structs/main.go
@@ -1,9 +1,28 @@
 package main
 
+import "fmt"
+
 func main() {
-	alex := person{"Alex", "Anderson"}
+	alex := person{"Alex", "Anderson", contactInfo{"email", 12}}
 	println(alex.firstName)
 
-	ale := person{firstName: "Ale", lastName: "Juarez"}
-	println(ale.firstName + " " + ale.lastName)
+	ale := person{
+		firstName: "Ale",
+		lastName:  "Juarez",
+		contactInfo: contactInfo{"email",
+			12,
+		},
+	}
+	ale.setFirstName("Alejandra")
+	ale.print()
+
+	var juan person
+	//default empty strings
+	println(juan.firstName + " " + juan.lastName)
+
+	colors := map[string]string{
+		"red": "#fff0000",
+	}
+	fmt.Println(colors)
+
 }
--- structs/person.go
+++ structs/person.go
@@ -1,6 +1,23 @@
 package main
 
+import "fmt"
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
+}
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+func (p person) print() {
+
+	fmt.Printf("%+v", p)
+}
+
+func (p *person) setFirstName(name string) {
+	(*p).firstName = name
 }
